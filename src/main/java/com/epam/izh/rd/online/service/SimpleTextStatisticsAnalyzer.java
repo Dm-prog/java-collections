6a9e39b5fd2774f84f3e2b145e383928970f3a8b
Dm-prog <prostokota@list.ru>
2021-09-06 15:00:35 +0300

    Курс Epam. Модуль: Работа с коллекциями в Java (Collections, Generics, Stream Api)

---
+++ src/main/java/com/epam/izh/rd/online/service/SimpleTextStatisticsAnalyzer.java
@@ -0,0 +1,140 @@
+package com.epam.izh.rd.online.service;
+
+import com.epam.izh.rd.online.helper.Direction;
+
+import java.util.*;
+
+/**
+ * Совет:
+ * Начните с реализации метода {@link SimpleTextStatisticsAnalyzer#getWords(String)}.
+ * Затем переиспользуйте данный метод при реализации других.
+ * <p>
+ * При необходимости, можно создать внутри данного класса дополнительные вспомогательные приватные методы.
+ */
+public class SimpleTextStatisticsAnalyzer implements TextStatisticsAnalyzer {
+
+    private SimpleTextStatisticsAnalyzer analyzer;
+
+    /**
+     * Необходимо реализовать функционал подсчета суммарной длины всех слов (пробелы, знаким препинания итд не считаются).
+     * Например для текста "One, I - tWo!!" - данный метод должен вернуть 7.
+     *
+     * @param text текст
+     */
+    @Override
+    public int countSumLengthOfWords(String text) {
+        analyzer = new SimpleTextStatisticsAnalyzer();
+        List<String> strings = analyzer.getWords(text);
+        int count = 0;
+        for (String s : strings) {
+            count += s.length();
+        }
+        return count;
+    }
+
+    /**
+     * Необходимо реализовать функционал подсчета количества слов в тексте.
+     * Например для текста "One, two, three, three - one, tWo, tWo!!" - данный метод должен вернуть 7.
+     *
+     * @param text текст
+     */
+    @Override
+    public int countNumberOfWords(String text) {
+        analyzer = new SimpleTextStatisticsAnalyzer();
+        List<String> strings = analyzer.getWords(text);
+        int count = 0;
+        for (String s : strings) {
+            count++;
+        }
+        return count;
+    }
+
+    /**
+     * Необходимо реализовать функционал подсчета количества уникальных слов в тексте (с учетом регистра).
+     * Например для текста "One, two, three, three - one, tWo, tWo!!" - данный метод должен вернуть 5.
+     * param text текст
+     */
+    @Override
+    public int countNumberOfUniqueWords(String text) {
+        analyzer = new SimpleTextStatisticsAnalyzer();
+        List<String> strings = analyzer.getWords(text);
+        Set<String> setStrings = new HashSet<>(strings);
+        int count = 0;
+        for (String s : setStrings) {
+            count++;
+        }
+        return count;
+    }
+
+    /**
+     * Необходимо реализовать функционал получения списка слов из текста.
+     * Пробелы, запятые, точки, кавычки и другие знаки препинания являются разделителями слов.
+     * Например для текста "One, two, three, three - one, tWo, tWo!!" должен вернуться список :
+     * {"One", "two", "three", "three", "one", "tWo", "tWo"}
+     *
+     * @param text текст
+     */
+    @Override
+    public List<String> getWords(String text) {
+        String[] res = text.split("[\\p{Punct}\\s]+");
+        List<String> wordsLine = Arrays.asList(res);
+        return wordsLine;
+    }
+
+    /**
+     * Необходимо реализовать функционал получения списка уникальных слов из текста.
+     * Пробелы, запятые, точки, кавычки и другие знаки препинания являются разделителями слов.
+     * Например для текста "One, two, three, three - one, tWo, tWo!!" должен вернуться список :
+     * {"One", "two", "three", "one", "tWo"}
+     *
+     * @param text текст
+     */
+    @Override
+    public Set<String> getUniqueWords(String text) {
+        analyzer = new SimpleTextStatisticsAnalyzer();
+        List<String> strings = analyzer.getWords(text);
+        Set<String> setStrings = new HashSet<>(strings);
+        return setStrings;
+    }
+
+    /**
+     * Необходимо реализовать функционал подсчета количества повторений слов.
+     * Например для текста "One, two, three, three - one, tWo, tWo!!" должны вернуться результаты :
+     * {"One" : 1, "two" : 1, "three" : 2, "one" : 1, "tWo" : 2}
+     *
+     * @param text текст
+     */
+    @Override
+    public Map<String, Integer> countNumberOfWordsRepetitions(String text) {
+        analyzer = new SimpleTextStatisticsAnalyzer();
+        Map<String, Integer> map = new TreeMap<>();
+        List<String> strings = analyzer.getWords(text);
+        for (String s : strings) {
+            int numberOfRepetitions = map.getOrDefault(s, 0) + 1;
+            map.put(s, numberOfRepetitions);
+        }
+        return map;
+    }
+
+    /**
+     * Необходимо реализовать функционал вывода слов из текста в отсортированном виде (по длине) в зависимости от параметра direction.
+     * Например для текста "Hello, Hi, mother, father - good, cat, c!!" должны вернуться результаты :
+     * ASC : {"mother", "father", "Hello", "good", "cat", "Hi", "c"}
+     * DESC : {"c", "Hi", "cat", "good", "Hello", "father", "mother"}
+     *
+     * @param text текст
+     */
+    @Override
+    public List<String> sortWordsByLength(String text, Direction direction) {
+        analyzer = new SimpleTextStatisticsAnalyzer();
+        List<String> strings = analyzer.getWords(text);
+        if (direction.equals(Direction.ASC)) {
+            Comparator<String> comparing = Comparator.comparing(String::length);
+            Collections.sort(strings, comparing);
+        } else if (direction.equals(Direction.DESC)) {
+            Comparator<String> reversed = Comparator.comparing(String::length).reversed();
+            Collections.sort(strings, reversed);
+        }
+        return strings;
+    }
+}
---
+++ src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
@@ -0,0 +1,67 @@
+package com.epam.izh.rd.online.service;
+
+import com.epam.izh.rd.online.helper.Direction;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Данный класс обязан использовать StreamApi из функционала Java 8. Функциональность должна быть идентична
+ * {@link SimpleTextStatisticsAnalyzer}.
+ */
+public class StreamApiTextStatisticsAnalyzer implements TextStatisticsAnalyzer {
+    private SimpleTextStatisticsAnalyzer analyzer;
+
+    @Override
+    public int countSumLengthOfWords(String text) {
+        analyzer = new SimpleTextStatisticsAnalyzer();
+        List<String> strings = analyzer.getWords(text);
+        return strings.stream().map(String::length)
+                .reduce(0, (totalLength, wordLength) -> totalLength + wordLength);
+    }
+
+    @Override
+    public int countNumberOfWords(String text) {
+        analyzer = new SimpleTextStatisticsAnalyzer();
+        List<String> strings = analyzer.getWords(text);
+        return (int) strings.stream().count();
+    }
+
+    @Override
+    public int countNumberOfUniqueWords(String text) {
+        analyzer = new SimpleTextStatisticsAnalyzer();
+        List<String> strings = analyzer.getWords(text);
+        return (int) strings.stream().distinct().count();
+    }
+
+    @Override
+    public List<String> getWords(String text) {
+        return Arrays.stream(text.split("[\\p{Punct}\\s]+")).collect(Collectors.toList());
+    }
+
+    @Override
+    public Set<String> getUniqueWords(String text) {
+        analyzer = new SimpleTextStatisticsAnalyzer();
+        List<String> strings = analyzer.getWords(text);
+        return strings.stream().collect(Collectors.toSet());
+    }
+
+    @Override
+    public Map<String, Integer> countNumberOfWordsRepetitions(String text) {
+        analyzer = new SimpleTextStatisticsAnalyzer();
+        List<String> strings = analyzer.getWords(text);
+        return strings.stream().collect(Collectors.toMap(Function.identity(), s -> 1, Integer::sum));
+    }
+
+    @Override
+    public List<String> sortWordsByLength(String text, Direction direction) {
+        analyzer = new SimpleTextStatisticsAnalyzer();
+        Comparator<String> comparator = Comparator.comparingInt(String::length);
+        return analyzer.getWords(text)
+                .stream()
+                .sorted(direction.equals(Direction.ASC) ? comparator : comparator.reversed())
+                .collect(Collectors.toList());
+
+    }
+}
